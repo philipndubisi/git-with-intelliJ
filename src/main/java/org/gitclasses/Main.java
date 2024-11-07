4ecd250ce5e6b615e3c177e1b593f300839e691e
Philip <philipndubisi93@gmail.com>
2024-11-07 15:48:59 +0100

    fourth commit

--- src/main/java/org/gitclasses/Main.java
+++ src/main/java/org/gitclasses/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("My second commit");
         System.out.println("Third commit");
+        System.out.println("Fourth commit");
     }
 }
