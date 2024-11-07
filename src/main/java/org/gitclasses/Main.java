db4159d1061b6dc6b4d1805b886dacc23d0199ee
Philip <philipndubisi93@gmail.com>
2024-11-07 14:17:05 +0100

    Second master commit

--- src/main/java/org/gitclasses/Main.java
+++ src/main/java/org/gitclasses/Main.java
@@ -3,5 +3,6 @@ package org.gitclasses;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("My second commit");
     }
 }
